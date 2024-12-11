0c7f885009c0b8ab5ea43072d15c4cb88db9320a
Clara <calvara976@g.educaand.es>
2024-12-11 08:36:14 +0100

    godos terminados

--- REACT/godos/src/App.js
+++ REACT/godos/src/App.js
@@ -3,34 +3,35 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Component } from 'react';
 import Flashcard from './componentes/FlashcardComponent';
 import { GODOS } from './shared/datos';
-import { Button } from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      godos:GODOS,
+      godos: GODOS,
     }
   }
   render() {
-    const lista = GODOS.map(
-      (rey)=> <Flashcard 
-                key={rey.id}
-                imagen={rey.imagen} 
-                titulo={rey.nombre}
-                texto={rey.texto}
-              />
-    );
     return (
       <div className="App">
-        <Button>this.state.godos[0].nombre</Button>
-        {lista}
+        <DisplayGodos reyes={this.state.godos}/>
       </div>
     );
   }
 }
 
-function DisplayGodos(props){
-  
+function DisplayGodos(props) {
+  const losreyes = props.reyes;
+  const lista = losreyes.map((rey) => <Flashcard
+    key={rey.id.toString()}
+    imagen={rey.imagen}
+    titulo={rey.nombre}
+    texto={rey.texto}
+  />);
+  return (
+    <ul>
+      {lista}
+    </ul>
+  );
 }
 export default App;
