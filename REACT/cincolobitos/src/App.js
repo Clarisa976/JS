2c3bdcc18d63d004b07f584fef2129351a7f29c2
Clara <calvara976@g.educaand.es>
2024-12-13 08:18:17 +0100

    .

--- REACT/cincolobitos/src/App.js
+++ REACT/cincolobitos/src/App.js
@@ -7,75 +7,48 @@ class App extends Component {
     super(props);
     this.state = {
       botoncitos: Array(5).fill('secondary'),
-      cuantos : 0
-    };
-  }
-
-  setSeleccionado(num){
-    let aux = this.state.botoncitos;
-    
-    if (aux[num] !=='danger') {
-      aux[num] ='danger';
-      this.setState({ cuantos: num + 1 });
-    }
-    this.setState({botoncitos:aux});
-    
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>{this.state.cuantos}</h1>
-          
-          <div>
-            <Botoncillo numBoton={0} color={this.state.botoncitos[0]} pulsar={(n)=>this.setSeleccionado(n)}/>
-            <Botoncillo numBoton={1} color={this.state.botoncitos[1]} pulsar={(n)=>this.setSeleccionado(n)}/>
-            <Botoncillo numBoton={2} color={this.state.botoncitos[2]} pulsar={(n)=>this.setSeleccionado(n)}/>
-            <Botoncillo numBoton={3} color={this.state.botoncitos[3]} pulsar={(n)=>this.setSeleccionado(n)}/>
-            <Botoncillo numBoton={4} color={this.state.botoncitos[4]} pulsar={(n)=>this.setSeleccionado(n)}/>
-          </div>
-        </header>
-      </div>
-    );
-  }
-}
-function Botoncillo(props) {
-  return (
-    // MUESTRA AQUÍ EL BOTÓN CON EL COLOR CORRESPONDIENTE
-    <>
-    
-    <Button color={props.color} onClick={()=>props.pulsar(props.numBoton)}></Button>
-    </>
-  );
-}
-export default App;
-
-
-
-/*EJERCICIO COMPLEJO:
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from 'reactstrap';
-import { Component } from 'react';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      botoncitos: Array(5).fill('secondary'),
       contadores: Array(5).fill(0)
     };
   }
 
   setSeleccionado(num) {
-    let aux = this.state.botoncitos;
-    aux[num] = 'danger';
+    let auxContador = this.state.contadores;
+    auxContador[num] +=1;
+
+    let contadorMaximo = auxContador.indexOf(Math.max(contadorMaximo));
+    let auxColor = this.state.botoncitos;
+    auxColor[num] = 'info';
+    this.setState({
+      botoncitos: auxColor,
+      contadores: auxContador
+    });
+    setTimeout(()=>{
+      let decrementar = auxContador.map((value,index)=>
+        index=== num && value > 0 ? value -1 : value);
+      let nuevoMaximoContador = decrementar.indexOf(Math.max(decrementar));
+      let updatedBotoncitos = Array(5).fill('secondary');
+      updatedBotoncitos[nuevoMaximoContador] = 'info';
+
+      this.setState({
+        botoncitos: decrementar,
+        contadores: updatedBotoncitos
+      },1000);
+      
+    })
+    /*let aux = this.state.botoncitos;
+    aux[num] = 'info';
     let auxContador = this.state.contadores;
     auxContador[num] += 1;
-    this.setState({ botoncitos: aux,
-      contadores: auxContador });
-
+    this.setState({
+      botoncitos: aux,
+      contadores: auxContador
+    });
+    setTimeout(() => {
+      let auxTime = this.state.botoncitos;
+      this.setState({botoncitos:auxTime});
+    },3000);*/
   }
+
 
   render() {
     return (
@@ -85,10 +58,10 @@ class App extends Component {
 
           <div>
             <Botoncillo numBoton={0} color={this.state.botoncitos[0]} pulsar={(n) => this.setSeleccionado(n)} contador={this.state.contadores[0]} />
-            <Botoncillo numBoton={1} color={this.state.botoncitos[1]} pulsar={(n) => this.setSeleccionado(n)} contador={this.state.contadores[1]}/>
-            <Botoncillo numBoton={2} color={this.state.botoncitos[2]} pulsar={(n) => this.setSeleccionado(n)} contador={this.state.contadores[2]}/>
-            <Botoncillo numBoton={3} color={this.state.botoncitos[3]} pulsar={(n) => this.setSeleccionado(n)}contador={this.state.contadores[3]} />
-            <Botoncillo numBoton={4} color={this.state.botoncitos[4]} pulsar={(n) => this.setSeleccionado(n)} contador={this.state.contadores[4]}/>
+            <Botoncillo numBoton={1} color={this.state.botoncitos[1]} pulsar={(n) => this.setSeleccionado(n)} contador={this.state.contadores[1]} />
+            <Botoncillo numBoton={2} color={this.state.botoncitos[2]} pulsar={(n) => this.setSeleccionado(n)} contador={this.state.contadores[2]} />
+            <Botoncillo numBoton={3} color={this.state.botoncitos[3]} pulsar={(n) => this.setSeleccionado(n)} contador={this.state.contadores[3]} />
+            <Botoncillo numBoton={4} color={this.state.botoncitos[4]} pulsar={(n) => this.setSeleccionado(n)} contador={this.state.contadores[4]} />
           </div>
         </header>
       </div>
@@ -105,7 +78,6 @@ function Botoncillo(props) {
 }
 export default App;
 
-*/
 /*
 Juan Carlos Moreno Perez
 9:40
