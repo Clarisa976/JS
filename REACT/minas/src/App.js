c7610bdd5b12b0b048091508f0e496741aa3602a
Clara <calvara976@g.educaand.es>
2025-01-07 10:09:45 +0100

    hacer los deberes para estudiar para morirme antes de lo que me espero morirme porque podria morirme ya pero aqui estamos sufriendo el paso del tiempo

--- REACT/minas/src/App.js
+++ REACT/minas/src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import { Component } from 'react';
 import { Button } from 'reactstrap';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={minas:0}
+  }
+  aumentar(){
+    let aux = this.state.minas+1;
+    this.setState({minas:aux})
+  }
+  disminuir(){
+    let aux = this.state.minas-1;
+    if(aux>=0){
+    this.setState({minas:aux})
+    }
+  }
+  render(){
+    return (
     <div className="App">
-      <Button>
-        Dale
-      </Button>
+      <div>
+      <h2>Minas: {this.state.minas}<Button onClick={()=>this.aumentar()}>+</Button><Button onClick={()=>this.disminuir()}>-</Button></h2>
+      </div>
+      <Button>Jugar</Button>
     </div>
   );
+  }
 }
 
 export default App;
