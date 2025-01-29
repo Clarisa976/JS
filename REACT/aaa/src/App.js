1385337ed1ff2571c420014a1feed8e602a3fb2b
Clarisa976 <clarisa21h@gmail.com>
2025-01-29 23:05:46 +0100

    .

--- REACT/aaa/src/App.js
+++ REACT/aaa/src/App.js
@@ -1,93 +1,83 @@
 import React, { Component, useState } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-const Atlas = ({ nuevoContacto }) => {
+import 'bootstrap/dist/css/bootstrap.min.css';
+const Altas = (props) => {
+  const { nuevoContacto } = props;
   // UTILICE HOOKS EN ESTE COMPONENTE
   const [nombre, setNombre] = useState('');
   const [apellidos, setApellidos] = useState('');
   const [telefono, setTelefono] = useState('');
 
-  const handleCambio = (e)=>{
-    if(e.target.name === 'nombre'){
+  //manejo de cambiar el estado
+  const handleChange = (e) => {
+    if (e.target.name === "nombre") {
       setNombre(e.target.value);
     }
-    if(e.target.name === 'apellidos'){
+    if (e.target.name === "apellidos") {
       setApellidos(e.target.value);
     }
-    if(e.target.name === 'telefono'){
+    if (e.target.name === "telefono") {
       setTelefono(e.target.value);
     }
   }
 
-  const handleSubmit =(e)=>{
+  //manejo del envio del formulario
+  const handleSubmit = (e) => {
     e.preventDefault();
-    nuevoContacto(nombre, apellidos, telefono);
+    if (nombre.trim() && apellidos.trim() && telefono.trim()) {
+      nuevoContacto(nombre, apellidos, telefono);
+    } else {
+      alert("no dejes campos vacíos");
+    }
   }
-  return(
+  return (
     <Form onSubmit={handleSubmit}>
-    <FormGroup>
-      <Label for="Nombre">Nombre:</Label>
-      <Input
-        name="nombre"
-        id="nombre"
-        placeholder="introduzca nombre"
-        onChange={handleCambio}
-      />
-      <Label for="Nombre">Apellidos:</Label>
-      <Input
-        name="apellidos"
-        id="apellidos"
-        placeholder="introduzca apellidos"
-        onChange={handleCambio}
-      />
-      <Label for="Nombre">Telefono:</Label>
-      <Input
-        name="telefono"
-        id="telefono"
-        placeholder="introduzca telefono"
-        onChange={handleCambio}
-      />
-    </FormGroup>
-    <Button>Añadir</Button>
-  </Form>
+      <FormGroup>
+        <Label for="Nombre">Nombre:</Label>
+        <Input name="nombre" id="nombre" placeholder="introduzca nombre" onChange={handleChange} />
+        <Label for="Nombre">Apellidos:</Label>
+        <Input name="apellidos" id="apellidos" placeholder="introduzca apellidos" onChange={handleChange} />
+        <Label for="Nombre">Telefono:</Label>
+        <Input name="telefono" id="telefono" type='number' placeholder="introduzca telefono" onChange={handleChange} />
+      </FormGroup>
+      <Button>Añadir</Button>
+    </Form>
   );
 }
-const Mostrar = ({ contactos, borrarContacto }) => {
-  // ESTE COMPONENTE MUESTRA EL LISTÍN TELEFÓNICO
+
+const Mostrar = (props) => {
+  // ESTE COMPONENTE MUESTRA EL LISTÍN TELEFÓNICO.
+  const { contactos, borrarContacto } = props;
   return (
     <ul>
-      {contactos.map(e => {
-        return (
-          <li>{e.nombre + " " + e.apellidos + " - " + e.telefono} <Button color='outline-info' onClick={() => borrarContacto(e.telefono)}>x</Button></li>
-        )
-      })}
+      {contactos.map(e => { return (<li>{e.nombre + ' ' + e.apellidos + ' ' + e.telefono + ' '}<Button onClick={() => borrarContacto(e.telefono)}>x</Button></li>) })}
     </ul>
-  )
+  );
+
 }
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // INSERTE AQUÑI EL ESTADO NECESARIO. AQUÍ SE GUARDARÁ TODA LA INFORMACIÓN DE LA APLICACIÓN. EL LISTÍN TELEFÓNICO.
+      // INSERTE AQUÍ EL ESTADO NECESARIO. AQUÍ SE GUARDARÁ TODA LA INFORMACIÓN DE LA APLICACIÓN. EL LISTÍN TELEFÓNICO
       contactos: [
-        { nombre: 'Koko', apellidos: 'Kokoro', telefono: '123456789' },
-        { nombre: 'Dio', apellidos: 'Diomedes', telefono: '123456798' }
+        { nombre: 'asdf', apellidos: 'zxcv', telefono: '123' }
       ],
-    }
+    };
   }
-
-  nuevoContacto = (nombre,apellido,telefono)=>{
+  //nuevoContacto
+  nuevoContacto(nombre, apellidos, telefono) {
     let copia = this.state;
     let aux = [];
-    if(copia.contactos.find(e=>e.telefono === telefono)=== undefined){
-      aux.nombre=nombre;
-      aux.apellidos=apellido;
-      aux.telefono=telefono;
+    if (copia.contactos.find(e => e.telefono === telefono) === undefined) {
+      aux.nombre = nombre;
+      aux.apellidos = apellidos;
+      aux.telefono = telefono;
       copia.contactos.push(aux);
     }
-    this.setState({copia})
+    this.setState({ copia });
   }
+  //borrarContacto
   borrarContacto = (telefono) => {
     let copia = this.state;
     let aux = []
@@ -102,16 +92,14 @@ class App extends Component {
     
     this.setState({copia})
   }
-
   render() {
     return (
-      <>
-      <Mostrar contactos={this.state.contactos}
-        borrarContacto={(telefono) => this.borrarContacto(telefono)} />
-      <Atlas nuevoContacto={(nombre,apellidos,telefono)=>this.nuevoContacto(nombre,apellidos,telefono)}/>
-      </>
+      <div className="App">
+        {/* DEBERÁ RENDERIZAR AL MENOS LOS DOS COMPONENTES ANTERIORES*/}
+        <Mostrar contactos={this.state.contactos} borrarContacto={(telefono) => this.borrarContacto(telefono)} />
+        <Altas nuevoContacto={(nombre, apellidos, telefono) => this.nuevoContacto(nombre, apellidos, telefono)} />
+      </div>
     );
   }
 }
-
 export default App;
